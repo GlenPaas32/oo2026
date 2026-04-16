0f7034a364cfb83115fde0d005f5a0bea02921f9
Glen Paas <glenpaas@tlu.ee>
2026-04-16 16:50:40 +0300

    Add files via upload

---
+++ Veebipood/src/main/java/ee/gpaas/veebipood/controller/OrderController.java
@@ -0,0 +1,42 @@
+package ee.gpaas.veebipood.controller;
+
+import ee.gpaas.veebipood.dto.OrderRowDto;
+import ee.gpaas.veebipood.entity.Order;
+import ee.gpaas.veebipood.entity.OrderRow;
+import ee.gpaas.veebipood.repository.OrderRepository;
+import ee.gpaas.veebipood.service.OrderService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@AllArgsConstructor
+public class OrderController {
+
+    private OrderRepository orderRepository;
+    private OrderService orderService;
+
+
+    @GetMapping("orders")
+    public List<Order> getOrders(){
+        return orderRepository.findAll();
+    }
+
+    @DeleteMapping("orders/{id}")
+    public List<Order> deleteOrder(@PathVariable Long id){
+        orderRepository.deleteById(id); // kustutan
+        return orderRepository.findAll(); // uuenenud seis
+    }
+
+    // person --> autentimise tokenist. parcelmachine --> Omnivast
+    // localhost:8080/orders?personId=1
+    @PostMapping("orders")
+    public Order addOrder(@RequestParam Long personId,
+                          @RequestParam(required = false) String parcelMachine,
+                          @RequestBody List<OrderRowDto> orderRows){
+        return orderService.saveOrder(personId, parcelMachine, orderRows); // siin salvestab
+        //return orderRepository.findAll(); // siin on uuenenud seis
+    }
+}
