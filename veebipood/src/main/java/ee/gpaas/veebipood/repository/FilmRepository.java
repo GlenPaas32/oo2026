d68723c1bc205a514e5c6a79270ed3b0bdea6a41
Glen Paas <glenpaas@tlu.ee>
2026-04-16 16:49:58 +0300

    Delete veebipood directory

--- veebipood/src/main/java/ee/gpaas/veebipood/repository/FilmRepository.java
+++
@@ -1,7 +0,0 @@
-//package ee.gpaas.veebipood.repository;
-//
-//import ee.gpaas.veebipood.entity.Film;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface FilmRepository extends JpaRepository<Film, Long> {
-//}
